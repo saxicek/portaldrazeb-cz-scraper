c1a384b58ef57ed04738622ebb2d6b841e1f83c4
sax <saxicek@gmail.com>
2016-01-14 22:01:23 +0100

    Fixed minor issues

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ var cloudant = require('./lib/cloudant');
 exports.handler = function(event, context) {
     switch (event.action) {
         case 'fetchAndParse':
-            cloudant.fetchAndParse();
+            cloudant.fetchAndParse()
+                .then(function() {
+                    context.succeed("Fetch and parse succeeded.");
+                });
             break;
         case 'addNewAuctionUrls':
-            cloudant.addNewAuctionUrls();
+            cloudant.addNewAuctionUrls()
+                .then(function() {
+                    context.succeed("Add new auction URLs succeeded.");
+                });
             break;
         default:
             console.log('Unsupported action ' + event.action);
